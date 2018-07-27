91628163082fd9e4427ea1d6c89590ce5b50f388
Ross Hinman <hinman.ross@gmail.com>
2018-07-27 15:52:20 -0400

    Initial commit of lab 10 without extra challenges

---
+++ Circle.Library/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle.Library
+{
+    public class Circle
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            double x = 2 * Math.PI * Radius;
+            return x;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            string formattedCircumference = FormatNumber(CalculateCircumference());
+            return formattedCircumference;
+        }
+
+        public double CalculateArea()
+        {
+            double x = Math.PI * Math.Pow(Radius, 2);
+            return x;
+        }
+        
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = FormatNumber(CalculateArea());
+            return formattedArea;
+        }
+
+        private string FormatNumber(double x)
+        {
+            string x1 = ($"{x:N2}");
+            return x1;
+        }
+        
+        private double Radius { get; set; }
+      
+
+    }
+}
---
+++ Lab_10_CIrcle/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Circle.Library;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10_CIrcle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Cricle Tester.");
+            bool repeat = true;
+            
+            do
+            {
+                for (int i = 1; i > 0; i++)
+                {
+
+                    int counter = i;
+                    Console.WriteLine("Enter Radius:");
+                    double radius = double.Parse(Console.ReadLine());
+                    var circleCalc = new Circle.Library.Circle(radius);
+
+                    var circumference = circleCalc.CalculateFormattedCircumference();
+                    var area = circleCalc.CalculateFormattedArea();
+
+
+                    Console.WriteLine($"Circumference: {circumference}");
+                    Console.WriteLine($"Area: {area}");
+                    
+                    Console.WriteLine("Would you like to continue: (y/n)");
+                    string answer = Console.ReadLine().ToLower();
+                    if (answer == "y")
+                    {
+                        repeat = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You created {counter} circle object(s). Goodbye.");
+                        repeat = false;
+                        i = -1;
+                    }
+                }
+            } while (repeat == true);
+
+            
+            Console.ReadKey();
+
+        }
+    }
+}
